624933f1ef4605b279a4542393fdf2506ec12740
sam bacha <sam@manifoldfinance.com>
2021-10-25 02:50:51 -0700

    refactor(repo): import private repos

---
+++ packages/libsol/hardhat.config.ts
@@ -0,0 +1,29 @@
+import '@typechain/hardhat';
+import 'dotenv/config';
+import { HardhatUserConfig } from 'hardhat/types';
+
+const mnemonic = 'test test test test test test test test test test test junk';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.7.4',
+    settings: {
+      metadata: {
+        bytecodeHash: 'none',
+      },
+      optimizer: {
+        enabled: true,
+        runs: 200,
+        details: {
+          yul: false,
+        },
+      },
+    },
+  },
+  typechain: {
+    outDir: 'src/',
+    target: 'ethers-v5',
+  },
+};
+
+export default config;
---
+++ shell.nix
@@ -0,0 +1,11 @@
+with import <nixpkgs> { };
+
+mkShell rec {
+  # include any libraries or programs in buildInputs
+  buildInputs = [
+  ];
+
+  # shell commands to be ran upon entering shell
+  shellHook = ''
+  '';
+}
