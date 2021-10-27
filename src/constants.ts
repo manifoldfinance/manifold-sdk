99c9abed7cc71f64d95bf639a812d76b8950d22a
sam bacha <sam@manifoldfinance.com>
2021-10-27 07:01:06 -0700

    fix(constants): declare bigint lib

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,22 @@
 // @FIXME - import protocol/constants and merge these two files
 
-
 import JSBI from 'jsbi';
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 
-export const INIT_CODE_HASH =
-  '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
 
-export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000);
+//***********************************************************************\\
+
+export type BigintIsh = JSBI | bigint | string
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
 
 // exports for internal consumption
 export const ZERO = JSBI.BigInt(0);
@@ -16,3 +24,18 @@ export const ONE = JSBI.BigInt(1);
 export const FIVE = JSBI.BigInt(5);
 export const _997 = JSBI.BigInt(997);
 export const _1000 = JSBI.BigInt(1000);
+
+//***********************************************************************\\
+
+
+export const FACTORY_ADDRESS = '';
+
+export const INIT_CODE_HASH = '';
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000);
+
+
+//***********************************************************************\\
+
+
+// @exports constants
